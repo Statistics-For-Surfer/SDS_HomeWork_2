8ae35d23c0085312e561ce7cee0ba035cd1e43ab
Matteo Candi <114286004+Matteo-Candi@users.noreply.github.com>
2022-12-28 19:36:13 +0100

    superfunction

--- main.R
+++ main.R
@@ -11,6 +11,7 @@
 # Load data ---------------------------------------------------------------
 rm(list=ls())
 load("~/Documents/GitHub/SDS_HomeWork_2/hw2_data.RData")
+load("hw2_data.RData")
 
 
 # Pool-Together -------------------------------------------------------------------------------------------------------------------------------------
@@ -35,107 +36,79 @@ hist(asd_sel$trinity_0050234$`2001`)
 
 # Association graph , Confidence intervals-----------------------------------------------------------------------------------------------------------------------------
 
-# only use one subjects for group since pool together isn't done
+# Function
+
+L_and_U <- function(data, type){
+  n <- dim(data)[1]
+  D <- dim(data)[2]
+  g <-  D - 2
+  alpha <- .05
+  m <- choose(D, 2)   # Binomial coefficient
+  
+  bon_alpha <- alpha / m  # Apply Bonferroni 
+  
+  correlation_matrix <- cor(data)
+  
+  Z_j_k_td <- (1/2)*log((1+correlation_matrix)/(1-correlation_matrix))
+  
+  #### Confidence intervals for theta
+  se <- sqrt(1/( n - 3))
+  
+  Log_lower <- Z_j_k_td - qnorm(1 - (bon_alpha/2)) * se
+  Log_upper <- Z_j_k_td + qnorm(1 - (bon_alpha/2)) * se
+  
+  #### Confidence intervals for rho
+  Lower_bound <- (exp(2*Log_lower) - 1 ) /   ((exp(2*Log_lower) + 1))
+  Upper_bound <- (exp(2*Log_upper) - 1 ) /   ((exp(2*Log_upper) + 1)) 
+  
+  Lower_bound[is.na(Lower_bound)] = 1
+  Upper_bound[is.na(Upper_bound)] = 1
+  
+  if(type == 'L'){
+    return(Lower_bound)
+  }
+  if(type == 'U'){
+    return(Upper_bound)
+  }
+}
+
+
+adj_matrix_func <- function(list, t){
+  lowers <- lapply(list, L_and_U, 'L')
+  uppers <- lapply(list, L_and_U, 'U')
+  
+  Y <- do.call(cbind, lowers)
+  Y <- array(Y, dim=c(dim(lowers[[1]]), length(lowers)))
+  LOWER <- apply(Y, c(1, 2), mean, na.rm = TRUE)
+  
+  Y <- do.call(cbind, uppers)
+  Y <- array(Y, dim=c(dim(uppers[[1]]), length(uppers)))
+  UPPER <- apply(Y, c(1, 2), mean, na.rm = TRUE)
+  
+  adj_matrix <- as.matrix(UPPER < -t | LOWER > t)
+  
+  colnames(adj_matrix) <- colnames(list[[1]])
+  rownames(adj_matrix) <- colnames(list[[1]])
+  
+  return(adj_matrix)
+}
+
+
+t <- 0.3
+adj_matrix_td <- adj_matrix_func(td_sel, t)
+adj_matrix_asd <- adj_matrix_func(asd_sel, t)
 
 
 
-
-
-
-td <- td_sel$caltech_0051487
-asd <- asd_sel$caltech_0051472
-
-dim(td)
-n <- dim(td)[1]
-D <- dim(td)[2]
-alpha <- .05
-m <-  choose(D, 2)   # Binomial coefficient
-m
-
-bon_alpha <- alpha / m 
-
-
-correlation_matrix_td <- cor(td)
-correlation_matrix_asd <- cor(asd)
-Z_j_k_td <- (1/2)*log((1+correlation_matrix_td)/(1-correlation_matrix_td))
-
-
-g <-  D - 2
-
-#### Confidence intervals for theta
-
-Log_lower <- Z_j_k_td - qnorm(1 - (bon_alpha/2)) * sqrt(1/( n - g - 3))
-Log_upper <- Z_j_k_td + qnorm(1 - (bon_alpha/2)) * sqrt(1/( n - g - 3))
-
-
-
-
-#### Confince intervals for rho
-
-Lower_bound <- (exp(2*Log_lower) - 1 ) /   ((exp(2*Log_lower) + 1))
-Upper_bound <- (exp(2*Log_upper) - 1 ) /   ((exp(2*Log_upper) + 1)) 
-
-Lower_bound[1,2]
-Upper_bound[1,2]
-
-
-# Build the adjcency matrix ---------------------------------------------------------------------------------------------------------------------
-
-# Idea: Set  a threshold t , if abs(t) is inside the interval ---> put an edge between the two ROI.
-t <- .3
-
-
-
-t <- .004
-
-
-adj_matrix <- as.matrix(Upper_bound < -t | Lower_bound > t)
-
-
-adj_matrix[is.na(adj_matrix)] = 1
-
-
-
-
-
-g <- graph.adjacency(adj_matrix, mode = "undirected", 
-                     diag = FALSE , )
+#library(igraph)
+g <- graph.adjacency(adj_matrix_td, mode = "undirected", diag = FALSE )
 set.seed(69)
-plot(g, vertex.size = 5 , vertex.size2 = 0.2, edge.size = 5)
+plot.igraph(g, vertex.size = 1, edge.size = 20)
 
 
-
-#install.packages("igraph")
-library(igraph)
-edges <- graph_from_adjacency_matrix(
-  adj_matrix,
-  mode = "undirected",
-  weighted = NULL,
-  diag = TRUE,
-  add.colnames = NULL,
-  add.rownames = NA
-)
-h <- graph.empty() + names(asd)
-h <- h + edges
-plot(h)
-
-
-# analysis together -----------------------------------------------------------------------------------------------------------------------------
-rm(list = ls())
-
-
-matrices <-  lapply(td_sel, cor)
-Y <- do.call(cbind, matrices)
-
-
-Y <- array(Y, dim=c(dim(matrices[[1]]), length(matrices)))
-
-
-mean_rho <- apply(Y, c(1, 2), mean, na.rm = TRUE)
-
-
-
-
+g <- graph.adjacency(adj_matrix_asd, mode = "undirected", diag = FALSE )
+set.seed(69)
+plot.igraph(g, vertex.size = 1, edge.size = 20)
 
 
 
--- matteo.r
+++ matteo.r
@@ -12,3 +12,87 @@ library(corrplot)
 data_cor <- cor(data)
 corrplot(data_cor, method='color', tl.pos='n')
 corrplot(data_cor, method='color', tl.pos='n', order="hclust")
+
+
+
+
+# Function
+
+L_and_U <- function(data, type){
+  n <- dim(data)[1]
+  D <- dim(data)[2]
+  g <-  D - 2
+  alpha <- .05
+  m <- choose(D, 2)   # Binomial coefficient
+  
+  bon_alpha <- alpha / m  # Apply Bonferroni 
+
+  correlation_matrix <- cor(data)
+  
+  Z_j_k_td <- (1/2)*log((1+correlation_matrix)/(1-correlation_matrix))
+  
+  #### Confidence intervals for theta
+  se <- sqrt(1/( n - 3))
+  
+  Log_lower <- Z_j_k_td - qnorm(1 - (bon_alpha/2)) * se
+  Log_upper <- Z_j_k_td + qnorm(1 - (bon_alpha/2)) * se
+
+  #### Confidence intervals for rho
+  Lower_bound <- (exp(2*Log_lower) - 1 ) /   ((exp(2*Log_lower) + 1))
+  Upper_bound <- (exp(2*Log_upper) - 1 ) /   ((exp(2*Log_upper) + 1)) 
+  
+  Lower_bound[is.na(Lower_bound)] = 1
+  Upper_bound[is.na(Upper_bound)] = 1
+  
+  if(type == 'L'){
+    return(Lower_bound)
+  }
+  if(type == 'U'){
+    return(Upper_bound)
+  }
+}
+
+adj_matrix_func <- function(list, t){
+  lowers <- lapply(list, L_and_U, 'L')
+  uppers <- lapply(list, L_and_U, 'U')
+  
+  Y <- do.call(cbind, lowers)
+  Y <- array(Y, dim=c(dim(lowers[[1]]), length(lowers)))
+  LOWER <- apply(Y, c(1, 2), mean, na.rm = TRUE)
+  
+  Y <- do.call(cbind, uppers)
+  Y <- array(Y, dim=c(dim(uppers[[1]]), length(uppers)))
+  UPPER <- apply(Y, c(1, 2), mean, na.rm = TRUE)
+  
+  adj_matrix <- as.matrix(UPPER < -t | LOWER > t)
+  
+  colnames(adj_matrix) <- colnames(list[[1]])
+  rownames(adj_matrix) <- colnames(list[[1]])
+
+  return(adj_matrix)
+}
+
+
+
+
+t <- 0.3
+adj_matrix_td <- adj_matrix_func(td_sel, t)
+adj_matrix_asd <- adj_matrix_func(asd_sel, t)
+
+
+
+#library(igraph)
+g <- graph.adjacency(adj_matrix_td, mode = "undirected", diag = FALSE )
+set.seed(69)
+plot.igraph(g, vertex.size = 1, edge.size = 20)
+
+
+g <- graph.adjacency(adj_matrix_asd, mode = "undirected", diag = FALSE )
+set.seed(69)
+plot.igraph(g, vertex.size = 1, edge.size = 20)
+
+
+
+
+
+
