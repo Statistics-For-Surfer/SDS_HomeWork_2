099a4ee1ea03edf0918a83d6b5601f345b6140b1
Matteo Candi <114286004+Matteo-Candi@users.noreply.github.com>
2022-12-29 16:10:36 +0100

    graphs

--- main.R
+++ main.R
@@ -36,6 +36,21 @@ hist(asd_sel$trinity_0050234$`2001`)
 
 
 
+rm(list=ls())
+
+
+# PACKAGES ----------------------------------------------------------------
+
+library(jpeg)
+library(ppcor)
+library(igraph)
+library(JointNets)
+
+
+
+# DATA --------------------------------------------------------------------
+
+load('hw2_data.RData')
 
 
 
@@ -58,8 +73,7 @@ lower_or_upper <- function(data, bound, cor_type='normal'){
     corr_matrix <- cor(data) }
   if(cor_type == 'partial'){
     g <- D-2
-    v <- 1 / diag(var(data))
-    corr_matrix <- - solve(cov(data)) / sqrt(v %*% t(v)) }
+    corr_matrix <- pcor(data) }
   
   #### Computing Fisher Z-Transform
   Z_j_k_td <- (1/2)*log((1+corr_matrix)/(1-corr_matrix))
@@ -121,7 +135,7 @@ cor_matrix_function <- function(list){
 
 # MATRICES ----------------------------------------------------------------
 
-t <- 0.03
+t <- 0.4
 
 td_adj_normal_cor <- adj_matrix_func(td_sel, t)
 td_adj_td_partial_cor <- adj_matrix_func(td_sel, t, 'partial')
@@ -134,19 +148,74 @@ asd_cor_matrix <- cor_matrix_function(asd_sel)
 
 
 
-
 # GRAPHS ------------------------------------------------------------------
 
-#library(igraph)
-g <- graph.adjacency(adj_matrix_td, mode = "undirected", diag = FALSE )
-set.seed(69)
-plot.igraph(g, vertex.size = 1, edge.size = 20)
+plot_graphs <- function(adj_mat_1, adj_mat_2,dimensions=2){
+  
+  
+  #### Create Graphs
+  g1 <- graph.adjacency(adj_mat_1, mode = "undirected", diag = FALSE )
+  g2 <- graph.adjacency(adj_mat_2, mode = "undirected", diag = FALSE )
+  
+  
+  #### Set nodes colors based on ROIs
+  colors <- c('aquamarine', 'chartreuse', 'darkorchid1', 'gold', 'lightpink1', 'orangered', 'salmon', 'seashell2', 'skyblue2')
+  
+  for(i in 1:length(V(g1)$name)){
+    V(g1)$color[i] <- colors[strtoi(substr(V(g1)$name[i],1,1))]
+    V(g2)$color[i] <- colors[strtoi(substr(V(g2)$name[i],1,1))] }
+  
+  
+  #### Change edges colors based on if they are present in both graphs or in only one
+  for(i in 1:length(E(g1))){
+    edge <- as_ids(E(g1)[i])
+    if(edge %in% as_ids(E(g2))){E(g1)[i]$color = 'black'}
+    else{E(g1)[i]$color = 'blue'} }
+  
+  
+  for(i in 1:length(E(g2))){
+    edge <- as_ids(E(g2)[i])
+    if(edge %in% as_ids(E(g1))){E(g2)[i]$color = 'black'}
+    else{E(g2)[i]$color = 'red'} }
+  
+  
+  #### Import ROI coordinates
+  data(aal116coordinates)
+  coord <- aal116coordinates
+  
+  if(dimensions == 2){
+    #### 2D Plot
+    par(mfrow=c(1,2))
+    layout <- matrix(c(coord$x.mni, coord$y.mni), 116,2)
+    
+    my_image <- readJPEG("brain.jpg")
+    
+    for(graph in list(g1,g2)){
+      
+      if(identical_graphs(graph, g1)){main = paste0("Brain's ROI correlation\n of TD patients (t=", t, ')')}
+      if(identical_graphs(graph, g2)){main = paste0("Brain's ROI correlation\n of ASD patients (t=", t, ')')}
+      
+      plot(0,0, type='n', xlim=c(-1.2, 1.2), ylim=c(-1.2, 1.2), axes=F, main=main, xlab='', ylab='')
+      
+      rasterImage(my_image, xleft=-1.2, xright=1.2, ybottom=-1.2, ytop=1.3)
+      
+      plot(graph, vertex.size=10, vertex.label.cex=.5, vertex.color=V(graph)$color, vertex.shape='circle',
+           edge.width=4, edge.color=E(graph)$color, vertex.label.col = 'black',
+           layout=layout, add=T) }}
+  
+  
+  if(dimensions == 3){
+    #### 3D Plot
+    layout <- matrix(c(coord$x.mni, coord$y.mni, coord$z.mni), 116,3)
+    
+    for(graph in list(g1, g2)){
+      rglplot(graph,
+              vertex.size=7, vertex.label.cex=.5, vertex.color=V(graph)$color,
+              edge.width=4, edge.color=E(graph)$color,
+              layout=layout, main=main) }}
+}
 
 
-g <- graph.adjacency(adj_matrix_asd, mode = "undirected", diag = FALSE )
-set.seed(69)
-plot.igraph(g, vertex.size = 1, edge.size = 20)
-
-
+plot_graphs(td_adj_normal_cor, asd_adj_normal_cor,dimensions=2)
 
 
