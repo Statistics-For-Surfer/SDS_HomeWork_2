e3ae1f995409c015946340faa8f98c82721387cc
Matteo Candi <114286004+Matteo-Candi@users.noreply.github.com>
2023-01-02 19:02:07 +0100

    all gifs

--- matteo.r
+++ matteo.r
@@ -26,9 +26,6 @@ rm(list=ls())
 library(jpeg)
 library(ppcor)
 library(igraph)
-library(htmlwidgets)
-library(animation)
-
 library(magick)
 library(dplyr)
 library(tidyr)
@@ -93,8 +90,6 @@ summary_dataset <- function(ls, metric='mean'){
 }
 
 
-TD <- summary_dataset(td_sel_scale, metric='mean')
-ASD <- summary_dataset(asd_sel_scale, metric='mean')
 
 
 
@@ -116,7 +111,8 @@ lower_or_upper <- function(data, bound, cor_type='normal', bonferroni=TRUE){
     corr_matrix <- cor(data) }
   if(cor_type == 'partial'){
     g <- D-2
-    corr_matrix <- pcor(data)$estimate}
+    corr_matrix <- pcor(data)$estimate
+  }
   
   #### Computing Fisher Z-Transform
   Z_j_k_td <- (1/2)*log((1+corr_matrix)/(1-corr_matrix))
@@ -134,8 +130,27 @@ lower_or_upper <- function(data, bound, cor_type='normal', bonferroni=TRUE){
   Lower_bound[is.na(Lower_bound)] = 1
   Upper_bound[is.na(Upper_bound)] = 1
   
-  if(bound == 'L'){ return(Lower_bound) }
-  if(bound == 'U'){ return(Upper_bound) }
+  if(bound == 'L'){ 
+    Log_lower <- Z_j_k_td - qnorm(1 - (alpha/2)) * se
+    
+    #### Confidence intervals for rho
+    Lower_bound <- (exp(2*Log_lower) - 1 ) /   ((exp(2*Log_lower) + 1))
+    
+    
+    #### Remove NA (on diagonal)
+    Lower_bound[is.na(Lower_bound)] = 1
+    
+    return(Lower_bound) }
+  if(bound == 'U'){ 
+    
+    Log_upper <- Z_j_k_td + qnorm(1 - (alpha/2)) * se
+    
+    #### Confidence intervals for rho
+    Upper_bound <- (exp(2*Log_upper) - 1 ) /   ((exp(2*Log_upper) + 1)) 
+    
+    #### Remove NA (on diagonal)
+    Upper_bound[is.na(Upper_bound)] = 1
+    return(Upper_bound) }
 }
 
 
@@ -152,9 +167,9 @@ plot_graphs <- function(mat_1, mat_2, t, dimensions=2, bonf=TRUE, cor_type='norm
   adj_mat_2 <- adj_matrix_func(mat_2, t = t, bonf=bonf, cor_type=cor_type)
   
   #Check if there are edges
-  if(sum(adj_mat_1) == 116 & sum(adj_mat_2)==116){
-    return('There are no edges. Try with lower value of t')
-  }
+  # if(sum(adj_mat_1) == 116 & sum(adj_mat_2)==116){
+  #   return('There are no edges. Try with lower value of t')
+  # }
   
   colnames(adj_mat_1) <- colnames(mat_1)
   colnames(adj_mat_2) <- colnames(mat_2)
@@ -162,8 +177,7 @@ plot_graphs <- function(mat_1, mat_2, t, dimensions=2, bonf=TRUE, cor_type='norm
   #### Create Graphs
   g1 <- graph.adjacency(adj_mat_1, mode = "undirected", diag = FALSE )
   g2 <- graph.adjacency(adj_mat_2, mode = "undirected", diag = FALSE )
-  
-  
+
   #### Set nodes colors based on ROIs
   colors <- c('0','#77DD77', '#C8A2C8', '#FFFF66', '#CB3234', '#E1E1E2', '#ff8c69', '#7fffd4', '#ABCDEF')
   
@@ -172,15 +186,18 @@ plot_graphs <- function(mat_1, mat_2, t, dimensions=2, bonf=TRUE, cor_type='norm
     V(g2)$color[i] <- colors[strtoi(substr(V(g2)$name[i],1,1))] }
   
   #### Change edges colors based on if they are present in both graphs or in only one
+  
+  if(sum(adj_mat_1) != 116){
   for(i in 1:length(E(g1))){
     edge <- as_ids(E(g1)[i])
     if(edge %in% as_ids(E(g2))){E(g1)[i]$color = 'black'}
-    else{E(g1)[i]$color = 'blue'} }
+    else{E(g1)[i]$color = 'blue'} }}
   
+  if(sum(adj_mat_2)!=116){
   for(i in 1:length(E(g2))){
     edge <- as_ids(E(g2)[i])
     if(edge %in% as_ids(E(g1))){E(g2)[i]$color = 'black'}
-    else{E(g2)[i]$color = 'red'} }
+    else{E(g2)[i]$color = 'red'} }}
   
   load('data/coordinates.RData')
   coord <- aal116coordinates
@@ -220,24 +237,52 @@ plot_graphs <- function(mat_1, mat_2, t, dimensions=2, bonf=TRUE, cor_type='norm
 
 
 
-plot_graphs(TD,ASD, .9, cor_type = 'partial')
+
+
+
+a_norm = lower_or_upper(TD , "L" , cor_type='normal', bonferroni=TRUE )
+b_norm =lower_or_upper(TD , "U" , cor_type='normal', bonferroni=TRUE )
+a_par = lower_or_upper(TD , "L" , cor_type='partial', bonferroni=TRUE )
+b_par = lower_or_upper(TD , "U" , cor_type='partial', bonferroni=TRUE )
+
+
+Ls_n <-sort(a_norm)
+US_n <- sort(b_norm)
+Ls <-sort(a_par)
+Us <- sort(b_par)
+
+
+xs = 1:length(Ls_n)
+
+par(mfrow=c(1,1))
+
+plot(Ls_n, col = "white" , main = "Asyntotic confidence intervals of rho \n 
+     with bonferroni  adjustment and without ASD", 
+     xlab = "" , ylab = "Lower and Upper Bound" , ylim = c(-1,1))
+
+
+segments(x0 = xs , y0 = Ls , x1 = xs , y1 = Us,  col = "gold")
+segments(x0 = xs , y0 = Ls_n , x1 = xs , y1 = US_n,  col = "lightblue")
+
+
+
 
 # GIFs --------------------------------------------------------------------
 
-gif_generator <- function(grid_t, cor_type, bonf, display=TRUE, save=TRUE){
+gif_generator <- function(grid_t, index, cor_type, bonf, display=TRUE, save=TRUE){
   if(bonf){ bonf <- 'bonf' } else{bonf <- 'no_bonf'}
   if(cor_type == 'normal'){cor_type <- 'normal'}
   if(cor_type == 'partial'){cor_type}
   
-  gif_path <- paste0("images/gifs/gif_", cor_type, "_cor_", bonf,"/")
+  gif_path <- paste0("images/gifs/", index,"/gif_", cor_type, "_cor_", bonf,"/")
   dir.create(file.path(gif_path), showWarnings = FALSE)
   unlink(paste0(gif_path, '*'))
   
   for(t in grid_t){
-    path <- paste0("images/gifs/gif_", cor_type, "_cor_", bonf, "/t=0_",t*100,".jpg")
-    if(t == .05){path <-  paste0("images/gifs/gif_", cor_type, "_cor_", bonf, "/t=0_0",t*100,".jpg")}
+    path <- paste0("images/gifs/", index,"/gif_", cor_type, "_cor_", bonf, "/t=0_",t*100,".jpg")
+    if(t > 0 & t < .1){path <-  paste0("images/gifs/", index, "/gif_", cor_type, "_cor_", bonf, "/t=0_0",t*100,".jpg")}
     jpeg(file=path, width = 900, height = 560)
-    plot_graphs(TD, ASD ,t = t , dimensions=2)
+    plot_graphs(TD, ASD , t = t, dimensions=2, cor_type = cor_type, bonf=bonf)
     dev.off()
   }
   
@@ -250,18 +295,18 @@ gif_generator <- function(grid_t, cor_type, bonf, display=TRUE, save=TRUE){
   
   if(save){
     image_write(image = img_animated,
-              path = paste0("images/gifs/brain_graph_", cor_type, "_cor_", bonf,".gif")) }
+              path = paste0("images/gifs/", index, "/brain_graph_", cor_type, "_cor_", bonf,".gif")) }
   
 }
 
 
-
-grid_t <- seq(0, 0.6, by=.05)
-
-gif_generator(grid_t, cor_type = 'normal', bonf = T, save = T, display=T)
+grid_t <- seq(0, 0.7, by=.05)
 
 
+TD <- summary_dataset(td_sel_scale, metric='sd')
+ASD <- summary_dataset(asd_sel_scale, metric='sd')
 
+gif_generator(grid_t, index='sd', cor_type = 'partial', bonf = F, save = T, display=T)
 
 
 
