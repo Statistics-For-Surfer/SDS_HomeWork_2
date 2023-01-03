cef803ef7c0e38a1d879353f9baa4a4803cc7f17
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-01-03 10:40:09 +0100

    updating report

--- paolo.r
+++ paolo.r
@@ -1,8 +1,24 @@
 rm(list=ls())
-??read_html
+
 # Importing data.
 load('data/hw2_data.RData')
 
+# Work on a time series of a single person.
+data <-  asd_sel$caltech_0051472
+
+# Each row is a time series and each column is a region of the brain
+head(data)
+library(corrplot)
+data_cor <- cor(data)
+corrplot(data_cor, method='color', tl.pos='n')
+corrplot(data_cor, method='color', tl.pos='n', order="hclust")
+
+
+
+
+
+
+rm(list=ls())
 
 
 # PACKAGES ----------------------------------------------------------------
@@ -10,6 +26,16 @@ load('data/hw2_data.RData')
 library(jpeg)
 library(ppcor)
 library(igraph)
+library(magick)
+library(dplyr)
+library(tidyr)
+
+
+
+# LOAD DATA ---------------------------------------------------------------
+
+load('data/hw2_data.RData')
+
 
 
 # DATA CLEANING -----------------------------------------------------------
@@ -64,13 +90,57 @@ summary_dataset <- function(ls, metric='mean'){
 }
 
 
-TD <- summary_dataset(td_sel_scale, metric='median')
-ASD <- summary_dataset(asd_sel_scale, metric='median')
+
+TD <- summary_dataset(td_sel_scale , metric = "median")
+ASD <- summary_dataset(asd_sel_scale, metric = "median")
+
+
+corr_distr_fun <- function(data_1 , data_2 , metric){
+  pTD  <-  summary_dataset(data_1 , metric = metric)
+  pASD <-  summary_dataset( data_2, metric = metric)
+  par(mfrow = c(1,2))
+  hist(cor(pTD) ,
+       main = "Histograms of correlation \n coefficients for TD subject",
+       xlab = expression(hat(rho)) , 
+       border = "white" , 
+       col = "#E52B50"  , freq = FALSE )
+  box()
+  hist(cor(pASD) ,
+       main = "Histograms of correlation\n coefficients for ASD subject",
+       xlab = expression(hat(rho)) , 
+       border = "white" , 
+       col = "#E52B50", freq = FALSE )
+  box()
+  par(mfrow = c(1,1))
+}
+
+corr_distr_fun(td_sel_scale , asd_sel_scale , metric = "median")
+corr_distr_fun(td_sel_scale , asd_sel_scale , metric = "sd")
 
 
 
 
-# FUNCTIONS ---------------------------------------------------------------
+quantile_distr_func <- function(data1 , data2 , metric){
+  pTD  <-  summary_dataset(data1 , metric = metric)
+  pASD <-  summary_dataset(data2, metric = metric)
+  q1   <-  sort(cor(pTD)) 
+  q2   <-  sort(cor(pASD)) 
+  par(mfrow = c(1,2))
+  plot(q1 , main = "Quantile distribution of correlation \n  coefficient for TD subject",
+       col = "#ffff66" , cex = .3)
+  grid()
+  plot(q2 , main = "Quantile distribution of correlation \n  coefficient for ASD subject",
+       col = "#ffff66" , cex = .3)
+  grid()
+  par(mfrow = c(1,1))
+}
+quantile_distr_func(td_sel_scale , asd_sel_scale , metric = "median")
+quantile_distr_func(td_sel_scale , asd_sel_scale , metric = "sd")
+
+
+
+
+
 
 lower_or_upper <- function(data, bound, cor_type='normal', bonferroni=TRUE){
   
@@ -90,7 +160,7 @@ lower_or_upper <- function(data, bound, cor_type='normal', bonferroni=TRUE){
   if(cor_type == 'partial'){
     g <- D-2
     corr_matrix <- pcor(data)$estimate
-    }
+  }
   
   #### Computing Fisher Z-Transform
   Z_j_k_td <- (1/2)*log((1+corr_matrix)/(1-corr_matrix))
@@ -108,80 +178,75 @@ lower_or_upper <- function(data, bound, cor_type='normal', bonferroni=TRUE){
   Lower_bound[is.na(Lower_bound)] = 1
   Upper_bound[is.na(Upper_bound)] = 1
   
-  if(bound == 'L'){ return(Lower_bound) }
-  if(bound == 'U'){ return(Upper_bound) }
+  if(bound == 'L'){ 
+    Log_lower <- Z_j_k_td - qnorm(1 - (alpha/2)) * se
+    
+    #### Confidence intervals for rho
+    Lower_bound <- (exp(2*Log_lower) - 1 ) /   ((exp(2*Log_lower) + 1))
+    
+    
+    #### Remove NA (on diagonal)
+    Lower_bound[is.na(Lower_bound)] = 1
+    
+    return(Lower_bound) }
+  if(bound == 'U'){ 
+    
+    Log_upper <- Z_j_k_td + qnorm(1 - (alpha/2)) * se
+    
+    #### Confidence intervals for rho
+    Upper_bound <- (exp(2*Log_upper) - 1 ) /   ((exp(2*Log_upper) + 1)) 
+    
+    #### Remove NA (on diagonal)
+    Upper_bound[is.na(Upper_bound)] = 1
+    return(Upper_bound) }
 }
 
 
-a <-lower_or_upper(TD , "L", cor_type='partial')
-a
-
-adj_matrix_func <- function(mat , t){
-  L <-  lower_or_upper(mat , "L")
-  U <-  lower_or_upper(mat , "U")
+adj_matrix_func <- function(mat , t, bonf=T, cor_type='normal'){
+  L <-  lower_or_upper(mat , "L", bonferroni=bonf, cor_type=cor_type)
+  U <-  lower_or_upper(mat , "U", bonferroni= bonf, cor_type=cor_type)
   adj <- as.matrix(L > t | U < -t)
   return(adj)
 }
 
 
-
-
-
-
-
-
-# MATRICES ----------------------------------------------------------------
-
-
-
-td_adj_normal_cor <- adj_matrix_func(td_sel_scale, t, bonferroni=T)
-asd_adj_normal_cor <- adj_matrix_func(asd_sel_scale, t, bonferroni=T)
-
-td_adj_td_partial_cor <- adj_matrix_func(td_sel_scale, t, 'partial')
-asd_adj_partial_cor <- adj_matrix_func(asd_sel_scale, t, 'partial')
-
-
-
-
-# GRAPHS ------------------------------------------------------------------
-
-plot_graphs <- function(mat_1 , mat_2 ,t, dimensions=2){
-  
-  adj_mat_1 <- adj_matrix_func(mat_1, t = t)
-  adj_mat_2 <- adj_matrix_func(mat_2, t = t)
+plot_graphs <- function(mat_1, mat_2, t, dimensions=2, bonf=TRUE, cor_type='normal'){
+  adj_mat_1 <- adj_matrix_func(mat_1, t = t, bonf=bonf, cor_type = cor_type)
+  adj_mat_2 <- adj_matrix_func(mat_2, t = t, bonf=bonf, cor_type=cor_type)
   
   #Check if there are edges
-  if(sum(adj_mat_1) == 116 & sum(adj_mat_2)==116){
-    return('There are no edges. Try with lower value of t')
-  }
+  # if(sum(adj_mat_1) == 116 & sum(adj_mat_2)==116){
+  #   return('There are no edges. Try with lower value of t')
+  # }
+  
+  colnames(adj_mat_1) <- colnames(mat_1)
+  colnames(adj_mat_2) <- colnames(mat_2)
   
   #### Create Graphs
   g1 <- graph.adjacency(adj_mat_1, mode = "undirected", diag = FALSE )
   g2 <- graph.adjacency(adj_mat_2, mode = "undirected", diag = FALSE )
   
-  
   #### Set nodes colors based on ROIs
-  colors <- c('aquamarine', 'chartreuse', 'darkorchid1', 'gold', 'lightpink1', 'orangered', 'salmon', 'seashell2', 'skyblue2')
+  colors <- c('0','#77DD77', '#C8A2C8', '#FFFF66', '#CB3234', '#E1E1E2', '#ff8c69', '#7fffd4', '#ABCDEF')
   
   for(i in 1:length(V(g1)$name)){
     V(g1)$color[i] <- colors[strtoi(substr(V(g1)$name[i],1,1))]
     V(g2)$color[i] <- colors[strtoi(substr(V(g2)$name[i],1,1))] }
   
-  
   #### Change edges colors based on if they are present in both graphs or in only one
-  for(i in 1:length(E(g1))){
-    edge <- as_ids(E(g1)[i])
-    if(edge %in% as_ids(E(g2))){E(g1)[i]$color = 'black'}
-    else{E(g1)[i]$color = 'blue'} }
   
+  if(sum(adj_mat_1) != 116){
+    for(i in 1:length(E(g1))){
+      edge <- as_ids(E(g1)[i])
+      if(edge %in% as_ids(E(g2))){E(g1)[i]$color = 'black'}
+      else{E(g1)[i]$color = 'blue'} }}
   
-  for(i in 1:length(E(g2))){
-    edge <- as_ids(E(g2)[i])
-    if(edge %in% as_ids(E(g1))){E(g2)[i]$color = 'black'}
-    else{E(g2)[i]$color = 'red'} }
+  if(sum(adj_mat_2)!=116){
+    for(i in 1:length(E(g2))){
+      edge <- as_ids(E(g2)[i])
+      if(edge %in% as_ids(E(g1))){E(g2)[i]$color = 'black'}
+      else{E(g2)[i]$color = 'red'} }}
   
-  
-  #### Import ROI coordinates
   load('data/coordinates.RData')
   coord <- aal116coordinates
   
@@ -215,90 +280,82 @@ plot_graphs <- function(mat_1 , mat_2 ,t, dimensions=2){
               vertex.size=7, vertex.label.cex=.5, vertex.color=V(graph)$color,
               edge.width=4, edge.color=E(graph)$color,
               layout=layout, main=main) }}
-}
-
-
-plot_graphs(TD, ASD ,t = .2 , dimensions=2 )
-
-plot_frame <- function(i) {
-  plot_graphs(TD, ASD ,t = i , dimensions=2)
   
 }
-ani.options(interval = 0.5, nmax = 50, loop = TRUE)
-
-th <- seq(from = 0 , to = .5 , by = .1)
-
-
-par(mfrow= c( 1, 1))
-
-Ls <- sort(lower_or_upper(TD , "L"))
-xs <- seq(from = 0 , to = length(Ls) , by = 1)
-Us <- sort(lower_or_upper(TD , "U") )
-Ls_n <-sort(lower_or_upper(TD , "L" , bonferroni = F))  
-US_n <- sort(lower_or_upper(TD , "U" , bonferroni = F))  
-
-plot(Ls, col = "white" , main = "Asyntotic confidence intervals of rho \n 
-     with bonferroni  adjustment and without ", 
-     xlab = "" , ylab = "Lower and Upper Bound")
-points(Us, , col = "white")
-segments(x0 = xs , y0 = Ls , x1 = xs , y1 = Us, , col = "lightblue")
-segments(x0 = xs , y0 = Ls_n , x1 = xs , y1 = US_n, , col = "#FFFF66")
-grid()
-points(sort(cor(TD)), col = "#CB3234", cex =  .3)
-legend("topleft" , c("With Bonferroni correction" , "Without Bonferroni correction") , col = c("lightblue" , "#FFFF66") , lty = 1 , lwd = 2,  border = "white")
-
-
-Ls_bon <- sort(lower_or_upper(ASD , "L"))
-Us_bon <- sort(lower_or_upper(ASD , "U"))
-Ls_n <-sort(lower_or_upper(ASD, "L" , bonferroni = F))  
-US_n <- sort(lower_or_upper(ASD , "U" , bonferroni = F))  
-rhos <- sort(cor(ASD))
-rhos <- rhos[rhos != 1]
-rhos <- unique(rhos)
-
-
-
-
-Ls_n <- Ls_n[Ls_n != 1]
-Ls_n <- unique(Ls_n)
-US_n <- US_n[US_n]
-US_n <- unique(US_n)
-Ls_bon <- Ls_bon[Ls_bon != 1]
-Ls_bon <- unique(Ls_bon)
-Us_bon <- Us_bon[Us_bon != 1]
-Us_bon <- unique(Us_bon)
-
-
-
-xs <- seq(from = 0 , to = length(Ls) , by = 1)
 
 
 
 
 
-plot(Ls, col = "white" , main = "Asyntotic confidence intervals of rho \n 
+
+a_norm = lower_or_upper(TD , "L" , cor_type='normal', bonferroni=TRUE )
+b_norm =lower_or_upper(TD , "U" , cor_type='normal', bonferroni=TRUE )
+a_par = lower_or_upper(TD , "L" , cor_type='partial', bonferroni=TRUE )
+b_par = lower_or_upper(TD , "U" , cor_type='partial', bonferroni=TRUE )
+
+
+Ls_n <-sort(a_norm)
+US_n <- sort(b_norm)
+Ls <-sort(a_par)
+Us <- sort(b_par)
+
+
+xs = 1:length(Ls_n)
+
+par(mfrow=c(1,1))
+
+plot(Ls_n, col = "white" , main = "Asyntotic confidence intervals of rho \n 
      with bonferroni  adjustment and without ASD", 
      xlab = "" , ylab = "Lower and Upper Bound" , ylim = c(-1,1))
-points(Us, , col = "white")
-
-segments(x0 = xs , y0 = Ls_bon , x1 = xs , y1 = Us_bon, , col = "lightblue")
-segments(x0 = xs , y0 = Ls_n , x1 = xs , y1 = US_n, , col = "#FFFF66")
-grid()
-points(rhos, col = "#CB3234", cex =  .3)
-legend("topleft" , c("With Bonferroni correction" , "Without Bonferroni correction") , col = c("lightblue" , "#FFFF66") , lty = 1 , lwd = 2,  border = "white")
 
 
-?cor.test
-?pcor
-??pcor
-a_prova <- pcor(TD , method = "pearson")$estimate
-dim(a_prova)
-dim(a_prova$estimate)
-a$
-?pcor
+segments(x0 = xs , y0 = Ls , x1 = xs , y1 = Us,  col = "gold")
+segments(x0 = xs , y0 = Ls_n , x1 = xs , y1 = US_n,  col = "lightblue")
 
-a$statistic
-a$statistic
-pcor(ASD)
 
--2.33 * sqrt(.5**2 / 200) + .5
+
+
+# GIFs --------------------------------------------------------------------
+
+gif_generator <- function(grid_t, index, cor_type, bonf, display=TRUE, save=TRUE){
+  if(bonf){ bonf <- 'bonf' } else{bonf <- 'no_bonf'}
+  if(cor_type == 'normal'){cor_type <- 'normal'}
+  if(cor_type == 'partial'){cor_type}
+  
+  gif_path <- paste0("images/gifs/", index,"/gif_", cor_type, "_cor_", bonf,"/")
+  dir.create(file.path(gif_path), showWarnings = FALSE)
+  unlink(paste0(gif_path, '*'))
+  
+  for(t in grid_t){
+    path <- paste0("images/gifs/", index,"/gif_", cor_type, "_cor_", bonf, "/t=0_",t*100,".jpg")
+    if(t > 0 & t < .1){path <-  paste0("images/gifs/", index, "/gif_", cor_type, "_cor_", bonf, "/t=0_0",t*100,".jpg")}
+    jpeg(file=path, width = 900, height = 560)
+    plot_graphs(TD, ASD , t = t, dimensions=2, cor_type = cor_type, bonf=bonf)
+    dev.off()
+  }
+  
+  imgs <- list.files(gif_path, full.names = TRUE)
+  img_list <- lapply(imgs, image_read)
+  img_joined <- image_join(img_list)
+  img_animated <- image_animate(img_joined, fps = 1)
+  
+  if(display){ image_browse(img_animated) } 
+  
+  if(save){
+    image_write(image = img_animated,
+                path = paste0("images/gifs/", index, "/brain_graph_", cor_type, "_cor_", bonf,".gif")) }
+  
+}
+
+
+grid_t <- seq(0, 0.7, by=.05)
+
+
+TD <- summary_dataset(td_sel_scale, metric='sd')
+ASD <- summary_dataset(asd_sel_scale, metric='sd')
+
+gif_generator(grid_t, index='sd', cor_type = 'partial', bonf = F, save = T, display=T)
+
+
+curve(qnorm(x))
+
